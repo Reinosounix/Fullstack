0b71746e67a11dd93ce855141b36723494861f9a
Reinosounix <54409170+Reinosounix@users.noreply.github.com>
2022-03-08 12:46:29 -0300

    JavaScript Lessons

---
+++ JavaScript lessons/Lesson 1/test.js
@@ -0,0 +1,7 @@
+var sentence = document.getElementById("title");
+sentence.innerHTML = "This is another title";
+var para = "This is a paragraph";
+var text = "This is a concat text";
+var sentence2 = document.getElementById("text");
+sentence2.innerHTML = para +" "+ text;
+
