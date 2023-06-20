8e9d78b903eac1db80747a86a4c840b9ba3e6128
Reinosounix <54409170+Reinosounix@users.noreply.github.com>
2023-06-20 15:28:53 -0400

    .net Swagger

---
+++ FrontEnd/javascript/index.js
@@ -0,0 +1,17 @@
+function updatemenu() {
+  if (document.getElementById("responsive-menu").checked == true) {
+    document.getElementById("menu").style.borderBottomRightRadius = "0";
+    document.getElementById("menu").style.borderBottomLeftRadius = "0";
+  } else {
+    document.getElementById("menu").style.borderRadius = "10px";
+  }
+}
+
+$('.openmodale').click(function (e) {
+    e.preventDefault();
+    $('.modale').addClass('opened');
+});
+$('.closemodale').click(function (e) {
+    e.preventDefault();
+    $('.modale').removeClass('opened');
+});
